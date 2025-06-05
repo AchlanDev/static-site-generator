0057c9e390d6a18f094cdbb3a2de01b0bf0713e1
AchlanDev <achlandev@gmail.com>
2025-06-05 17:49:34 -0400

    Feat: Add static directory, public directory
    Update: Update .gitignore to ignore public directory
    
    Update: Update main.py to copy files from static to public

--- src/main.py
+++ src/main.py
@@ -2,10 +2,46 @@
 # Static Site Generator Main Module
 # Run: source venv/bin/activate in ./static-site-generator
 
+import os
+import shutil
 from textnode import TextNode, TextType
 
 def main():
-    print(TextNode("This is a test", TextType.LINK,  "http://example.com"))
-    
+    result = static_to_public("static", "public")
+
+    for item in os.walk("static"):
+        for name in item[2]:
+            print(os.path.join(item[0], name))
+
+    return result
+
+def static_to_public(static_dir, public_dir):
+
+    if os.path.exists(public_dir):
+        print(f"Removing existing public directory: {public_dir}")
+        shutil.rmtree(public_dir)
+    os.makedirs(public_dir)
+
+    def _copy_static_files(current_src, current_dst):
+
+        for item in os.listdir(current_src):
+
+            print(os.path.join(current_src, item))
+
+            src_path = os.path.join(current_src, item)
+            dst_path = os.path.join(current_dst, item)
+
+            if os.path.isdir(src_path):
+                print(f"Entering directory: {src_path}")
+                os.makedirs(dst_path, exist_ok=True)
+                _copy_static_files(src_path, dst_path)
+            else:
+                print(f"Copying {item} from {src_path} to {dst_path}")
+                shutil.copy2(src_path, dst_path)
+
+    _copy_static_files(static_dir, public_dir)
+
+
+
 if __name__ == "__main__":
     main()
